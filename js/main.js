5232408eb5d2f8acb5a9f3da83bfcdec29ec46ea
Maicol <mdmch13@gmail.com>
2023-08-03 10:25:56 -0300

    Se modifico index.html, styles.css y main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 let events = [];
+let arr = [];
 
 const eventName = document.querySelector("#eventName");
 const eventDate = document.querySelector("#eventDate");
-const buttoAdd = document.querySelector("#bAdd");
+const buttonAdd = document.querySelector("#bAdd");
+const eventsContainer = document.querySelector("#eventsContainer");
 
 const json = load();
-const arr = JSON.parse(json);
-events = [...arr];
+
+try{
+    arr = JSON.parse(json);
+} catch (error) {
+    arr = [];
+}
+events = arr ? [...arr] : [];
+
 renderEvents();
 
 document.querySelector("form").addEventListener("submit", (e) => {
     e.preventDefault();
+    addEvent();
 });
 
 buttonAdd.addEventListener("click", (e) => {
+    e.preventDefault();
     addEvent();
 });
 
@@ -21,47 +31,66 @@ function addEvent(){
     if (eventName.value === "" || eventDate.value === ""){
         return;
     }
-    if (datediff(eventDate.value) < 0){
+
+    if (dateDiff(eventDate.value) < 0){
         return;
     }
 
-
     const newEvent = {
-        id: (Math.random() * 100).toString(36).slice(2),
+        id: (Math.random() * 100).toString(36).slice(3),
         name: eventName.value,
         date: eventDate.value,
     };
 
     events.unshift(newEvent);
+
     save(JSON.stringify(events));
+
     eventName.value = "";
+
     renderEvents();
 }
 
 function renderEvents(){
     const eventsHTML = events.map((event) => {
         return `
-            <div class = "task">
+            <div class = "event">
                 <div class = "days">
-                    <span class = "days-number">${datediff(event.date)}</span>
+                    <span class = "days-number">${dateDiff(event.date)}</span>
                     <span class = "days-text">días</span>
                 </div>
                 <div class = "event-name">${event.name}</div>
                 <div class = "event-date">${event.date}</div>
                 <div class = "actions">
-                    <button data-id = "${event.id} class = "bDelete">Eliminar</button>
+                    <button data-id = "${event.id}" class = "bDelete">Eliminar</button>
                 </div>
             </div>`;
     });
 
-    document.querySelector("#tasksContainer").innerHTML = eventsHTML.join("");
+    eventsContainer.innerHTML = eventsHTML.join("");
 
     document.querySelectorAll(".bDelete").forEach((button) => {
         button.addEventListener("click", (e) => {
             const id = button.getAttribute("data-id");
             events = events.filter((event) => event.id !== id);
-            save();
+            save(JSON.stringify(events));
             renderEvents();
         });
     });
+}
+
+function dateDiff(d){
+    const targetDate =  new Date(d);
+    const today = new Date();
+    const difference = targetDate.getTime() - today.getTime();
+    const days = Math.ceil(difference / (1000 * 3600 *24));
+    return days;
+}
+
+function save(data){
+    localStorage.setItem("items",data);
+}
+
+function load(){
+    return localStorage.getItem("items");
 }
